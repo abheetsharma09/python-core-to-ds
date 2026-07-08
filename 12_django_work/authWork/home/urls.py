4df810594fd58b7e6eb26f581299252aea905fe5
ABHEET SHARMA <abheetsharma09@gmail.com>
2026-07-08 11:11:35 +0530

    Created working login system

--- 12_django_work/authWork/home/urls.py
+++ 12_django_work/authWork/home/urls.py
@@ -5,5 +5,7 @@ from home import views
 urlpatterns = [
     path("" , views.homePage , name="homePage"),
     path("signIn/" , views.signIn , name="signIn"),
-    path("logIn/" , views.logIn , name="logIn")
+    path("login/" , views.logIn , name="logIn"),
+    path("logout/" , views.logout_user, name="logout"),
+    path("dashboard/" , views.dashboard, name="dashboard")
 ]
--- 12_django_work/authWork/home/views.py
+++ 12_django_work/authWork/home/views.py
@@ -1,18 +1,50 @@
 from django.shortcuts import render , HttpResponse , redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate , logout , login
+from django.contrib import messages
 
 # Create your views here.
 def homePage(requests):
-    if requests.user.isanonymous:
-        redirect("/signIn")
-    return render(requests , "home.html")
+    if requests.user.is_authenticated:
+        return render(requests , "home.html")
+    else:
+        return redirect("login/")
+        
 
 def signIn(requests):
-    # return render(requests , 'signIn.html')
+    if requests.user.is_authenticated:
+        return redirect("/")
+    
+    if requests.method == "POST":
+        u_name = requests.POST.get("name")
+        u_username = requests.POST.get("username")
+        u_email = requests.POST.get("email")
+        u_password = requests.POST.get("password")
+        u_password_check = requests.POST.get("password-check")
+
+        if u_password != u_password_check:
+            messages.add_message(requests, messages.INFO, "Password does not match")
+            messages.add_message(requests, messages.INFO, "Retype Password")
+            return render(requests , "signIn.html")
+
+        if User.objects.filter(username=u_username).exists():
+            messages.add_message(requests, messages.INFO, "User already exists!!")
+            return redirect('/login')
+
+        user = User.objects.create_user(username=u_name, email=u_email, password=u_password)
+        user.first_name = u_name
+        user.save()
+
+        login(requests, user)
+
+        return redirect("/")
+
     return render(requests , "signIn.html")
 
 def logIn(requests):
+    if requests.user.is_authenticated:
+        return redirect("/")
+    
     if requests.method == "POST":
         username= requests.POST.get("username")
         password = requests.POST.get("password")
@@ -20,11 +52,18 @@ def logIn(requests):
         user = authenticate(username=username, password=password)
         if user is not None:
         # A backend authenticated the credentials
-            print("Correct Credentials")
-            redirect("/")
+            login(requests, user) 
+            messages.add_message(requests, messages.INFO, f"Welcome Back {username}")
+            return redirect("/")
         else:
         # No backend authenticated the credentials
-            print("Wrong Credentials")
+            messages.add_message(requests, messages.INFO, "Wrong Credentials..Try Again")
+            messages.add_message(requests, messages.INFO, "Click Forget Password??..")
 
         # checks if the user enter correct credentials
     return render(requests, "login.html")
+
+def logout_user(requests):
+    logout(requests)
+    messages.add_message(requests , messages.INFO , "Logout Sucessfull !!")
+    return redirect('/login')
